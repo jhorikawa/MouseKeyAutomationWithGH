573aa0b9e729f1613d6364351223fb82b3128b50
JH <jhorikawa@users.noreply.github.com>
2021-02-06 18:39:48 +0900

    Update windowhandler.cs

--- windowhandler.cs
+++ windowhandler.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Diagnostics;
 
 public class GrasshopperWindowHandler(){
     IntPtr GetGHWIndowHandle(GH_Document gh){
